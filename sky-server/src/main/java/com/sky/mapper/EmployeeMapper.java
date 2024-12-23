86d1d3813f3ac25ad669b7d54dba17bc5a083dd9
XXN <xu>
2024-12-23 17:51:57 +0800

    function:increase employee keyword: function:page query just using sql limit sql:select * from employee limit 0,10

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,12 +1,17 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
 import com.sky.entity.Employee;
+import com.sky.result.PageResult;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
+import java.util.List;
+
 @Mapper
 public interface EmployeeMapper {
+
 
     /**
      * 根据用户名查询员工
@@ -19,4 +24,6 @@ public interface EmployeeMapper {
     @Insert("INSERT INTO employee (name, username, password, phone, sex, id_number, status, create_time, update_time, create_user, update_user) " +
             "VALUES (#{name}, #{username}, #{password}, #{phone}, #{sex}, #{idNumber}, #{status}, #{createTime}, #{updateTime}, #{createUser}, #{updateUser})")
     void insert(Employee employee);
+
+    List<Employee> pageQuery(String name, int pageSize, int offset);
 }
--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -1,17 +1,21 @@
 package com.sky.service.impl;
 
 import com.alibaba.druid.support.spring.stat.annotation.Stat;
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import com.sky.constant.MessageConstant;
 import com.sky.constant.PasswordConstant;
 import com.sky.constant.StatusConstant;
 import com.sky.context.BaseContext;
 import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
 import com.sky.exception.AccountLockedException;
 import com.sky.exception.AccountNotFoundException;
 import com.sky.exception.PasswordErrorException;
 import com.sky.mapper.EmployeeMapper;
+import com.sky.result.PageResult;
 import com.sky.service.EmployeeService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +23,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
@@ -96,6 +101,22 @@ public class EmployeeServiceImpl implements EmployeeService {
 
 
 
+    }
+
+    /*
+     * function:page query just using sql limit
+     *
+     * @date 2024/12/23 17:49
+     * @param employeePageQueryDTO
+     * @return com.sky.result.PageResult
+     */
+    @Override
+    public PageResult pageQuery(EmployeePageQueryDTO employeePageQueryDTO) {
+        // sql: select * from employee limit 0,10
+        String employName = employeePageQueryDTO.getName();
+        int offset = (employeePageQueryDTO.getPage() - 1) * employeePageQueryDTO.getPageSize();
+        List<Employee> pageList = employeeMapper.pageQuery(employName, employeePageQueryDTO.getPage(), offset);
+        return new PageResult(pageList.size(), pageList);
     }
 
 }
