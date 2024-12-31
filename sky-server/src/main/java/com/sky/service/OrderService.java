8b6660c1d6808dd074402b34148bcc853703ce32
XXN <xu>
2024-12-31 21:55:59 +0800

    function: Import order manage function

--- sky-server/src/main/java/com/sky/service/OrderService.java
+++ sky-server/src/main/java/com/sky/service/OrderService.java
@@ -1,9 +1,11 @@
 package com.sky.service;
 
-import com.sky.dto.OrdersPaymentDTO;
-import com.sky.dto.OrdersSubmitDTO;
+import com.sky.dto.*;
+import com.sky.result.PageResult;
 import com.sky.vo.OrderPaymentVO;
+import com.sky.vo.OrderStatisticsVO;
 import com.sky.vo.OrderSubmitVO;
+import com.sky.vo.OrderVO;
 
 /**
  * @author xuxunne
@@ -26,6 +28,49 @@ public interface OrderService {
       * @param outTradeNo
       */
      void paySuccess(String outTradeNo);
+
+     /**
+      * 用户端订单分页查询
+      * @param page
+      * @param pageSize
+      * @param status
+      * @return
+      */
+     PageResult pageQuery4User(int page, int pageSize, Integer status);
+
+     /**
+      * 查询订单详情
+      * @param id
+      * @return
+      */
+     OrderVO details(Long id);
+
+     /**
+      * 用户取消订单
+      * @param id
+      */
+     void userCancelById(Long id) throws Exception;
+
+     /**
+      * 再来一单
+      *
+      * @param id
+      */
+     void repetition(Long id);
+
+     PageResult conditionSearch(OrdersPageQueryDTO ordersPageQueryDTO);
+
+     OrderStatisticsVO statistics();
+
+     void confirm(OrdersCancelDTO ordersCancelDTO);
+
+     void rejection(OrdersRejectionDTO ordersRejectionDTO) throws Exception;
+
+     void cancel(OrdersCancelDTO ordersCancelDTO)  throws Exception;
+
+     void delivery(Long id);
+
+     void complete(Long id);
 }
 
 
